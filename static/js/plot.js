b1370c5de261cac9cfcbeeec9866be0f7e981e19
unknown <henrywrandall@gmail.com>
2020-07-07 18:19:40 -0400

    added lines for the order dates

--- static/js/plot.js
+++ static/js/plot.js
@@ -83,17 +83,21 @@ function StateGraph(stateName,type,variable) {
   if ((type) == 'Total') {
     if ((variable) == 'Cases') {
       var df=state_cases;
+      var label = 'Total Cases';
     }
     else {
       var df=state_deaths;
+      var label = 'Total Deaths';
     }
   }
   else {
     if ((variable) == 'Cases') {
       var df=state_cases_daily;
+      var label = 'Reported Cases';
     }
     else {
       var df=state_deaths_daily;
+      var label = 'Reported Deaths';
     }
   }
   
@@ -111,19 +115,52 @@ function StateGraph(stateName,type,variable) {
     values.push(data[dates[date]]);
   }
 
-  var chartData = {
-    labels: dates,
-    datasets: [{
-      data: values,
-    }]
+  orderData=JSON.parse(JSON.stringify(orders[index]));
+  stateOrders=Object.keys(orderData);
+  var orderDates=[];
+  for (var orderDate in stateOrders) {
+    orderDates.push(orderData[stateOrders[orderDate]]);
   };
+  
+  var orderImplementationtest= (new Date(orderDates[1])).toLocaleDateString('en-US');
+  var orderExpirationtest= (new Date(orderDates[2])).toLocaleDateString('en-US');
 
-  $('#Splot').remove(); // this is my <canvas> element
+
+  window.orderImplementation = undefined;
+  window.orderExpiration = undefined;
+  for (var date in dates) {
+    today=(new Date(dates[date])).toLocaleDateString('en-US');
+    if (today == orderImplementationtest) {
+      window.orderImplementation = date;
+    }
+    if (today == orderExpirationtest) {
+      window.orderExpiration = date;
+      break;
+    }
+  }
+
+  $('#Splot').remove();
   $('#Sgraph-container').html('<canvas id="Splot"></canvas>');
-  var Splot = document.getElementById("Splot");
-  new Chart(Splot, {
+
+  var myLineExtend = Chart.controllers.line.prototype.draw;
+  var ctx = document.getElementById("Splot").getContext("2d");
+  var config = {
     type: 'line',
-    data: chartData,
+    data: {
+      labels: dates,
+      datasets: [{
+        label: label,
+        fill: false,
+        borderColor: "#000000",
+        pointBackgroundColor: "#82CAFA",
+        pointBorderColor: "#000000",
+        pointHoverBackgroundColor: "#0000FF",
+        data: values
+      }],
+      datasetFill: false,
+      lineAtIndex1: orderImplementation,
+      lineAtIndex2: orderExpiration
+    },
     options: {
       scales: {
         yAxes: [{
@@ -136,24 +173,70 @@ function StateGraph(stateName,type,variable) {
         display: false
       }
     }
+  };
+
+  Chart.helpers.extend(Chart.controllers.line.prototype, {
+    draw: function () {
+    
+      myLineExtend.apply(this, arguments);   
+
+      var chart = this.chart;
+      var ctx = chart.chart.ctx;
+
+      var index1 = chart.config.data.lineAtIndex1;
+      var index2 = chart.config.data.lineAtIndex2;
+      var xaxis = chart.scales['x-axis-0'];
+      var yaxis = chart.scales['y-axis-0'];
+
+      ctx.save();
+      ctx.beginPath();
+      ctx.moveTo(xaxis.getPixelForValue(undefined, index1), yaxis.top + 24);
+      ctx.strokeStyle = '#ff0000';
+      ctx.lineWidth= 3;
+      ctx.lineTo(xaxis.getPixelForValue(undefined, index1), yaxis.bottom);
+      ctx.stroke();
+      ctx.restore();
+
+      ctx.save();
+      ctx.beginPath();
+      ctx.moveTo(xaxis.getPixelForValue(undefined, index2), yaxis.top + 24);
+      ctx.strokeStyle = '#008000';
+      ctx.lineWidth= 3;
+      ctx.lineTo(xaxis.getPixelForValue(undefined, index2), yaxis.bottom);
+      ctx.stroke();
+      ctx.restore();
+
+      ctx.textAlign = 'end';
+      ctx.font="20px sans-serif";
+      ctx.fillStyle='#000000';
+      ctx.fillText("Quarantine Begin", xaxis.getPixelForValue(undefined, index1), yaxis.top + 16);
+      ctx.textAlign = 'start';
+      ctx.fillText("Quarantine End", xaxis.getPixelForValue(undefined, index2), yaxis.top + 16);
+    }
   });
+
+  new Chart(ctx, config);
 };
 
 function CountyGraph(stateName,countyName,type,variable) {
   if ((type) == 'Total') {
     if ((variable) == 'Cases') {
       var df=county_cases;
+      var label = 'Total Cases';
     }
     else {
       var df=county_deaths;
+      var label = 'Total Deaths';
     }
   }
   else {
     if ((variable) == 'Cases') {
       var df=county_cases_daily;
+      var label = 'Reported Cases';
     }
     else {
       var df=county_deaths_daily;
+      var label = 'Reported Deaths';
     }
   }
 
@@ -173,19 +256,28 @@ function CountyGraph(stateName,countyName,type,variable) {
     values.push(data[dates[date]]);
   }
 
-  var chartData = {
-    labels: dates,
-    datasets: [{
-      data: values,
-    }]
-  };
-
-  $('#Cplot').remove(); // this is my <canvas> element
+  $('#Cplot').remove();
   $('#Cgraph-container').html('<canvas id="Cplot"></canvas>');
-  var Cplot = document.getElementById("Cplot");
-  new Chart(Cplot, {
+
+  var myLineExtend = Chart.controllers.line.prototype.draw;
+  var ctx = document.getElementById("Cplot").getContext("2d");
+  var config = {
     type: 'line',
-    data: chartData,
+    data: {
+      labels: dates,
+      datasets: [{
+        label: label,
+        fill: false,
+        borderColor: "#000000",
+        pointBackgroundColor: "#82CAFA",
+        pointBorderColor: "#000000",
+        pointHoverBackgroundColor: "#0000FF",
+        data: values
+      }],
+      datasetFill: false,
+      lineAtIndex1: orderImplementation,
+      lineAtIndex2: orderExpiration
+    },
     options: {
       scales: {
         yAxes: [{
@@ -198,7 +290,10 @@ function CountyGraph(stateName,countyName,type,variable) {
         display: false
       }
     }
-  });
+  };
+
+  new Chart(ctx, config);
 };
+
 
 init();
