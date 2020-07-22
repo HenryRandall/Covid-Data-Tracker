304f6dcd0722e88aae8bbd5d7916b95d90e63839
unknown <henrywrandall@gmail.com>
2020-07-22 09:31:16 -0400

    added caching

--- app.py
+++ app.py
@@ -58,22 +58,7 @@ else:
         config={'CACHE_TYPE': 'saslmemcached',
                 'CACHE_MEMCACHED_SERVERS': cache_servers.split(','),
                 'CACHE_MEMCACHED_USERNAME': cache_user,
-                'CACHE_MEMCACHED_PASSWORD': cache_pass,
-                'CACHE_OPTIONS': { 'behaviors': {
-                    # Faster IO
-                    'tcp_nodelay': True,
-                    # Keep connection alive
-                    'tcp_keepalive': True,
-                    # Timeout for set/get requests
-                    'connect_timeout': 2000, # ms
-                    'send_timeout': 750 * 1000, # us
-                    'receive_timeout': 750 * 1000, # us
-                    '_poll_timeout': 2000, # ms
-                    # Better failover
-                    'ketama': True,
-                    'remove_failed': 1,
-                    'retry_timeout': 2,
-                    'dead_timeout': 30}}})
+                'CACHE_MEMCACHED_PASSWORD': cache_pass})
 
 
 # create route that renders index.html template
@@ -120,3 +105,21 @@ def aboutus():
 
 if __name__ == "__main__":
     app.run(debug=False)
+
+
+# ,
+#                 'CACHE_OPTIONS': { 'behaviors': {
+#                     # Faster IO
+#                     'tcp_nodelay': True,
+#                     # Keep connection alive
+#                     'tcp_keepalive': True,
+#                     # Timeout for set/get requests
+#                     'connect_timeout': 2000, # ms
+#                     'send_timeout': 750 * 1000, # us
+#                     'receive_timeout': 750 * 1000, # us
+#                     '_poll_timeout': 2000, # ms
+#                     # Better failover
+#                     'ketama': True,
+#                     'remove_failed': 1,
+#                     'retry_timeout': 2,
+#                     'dead_timeout': 30}}
