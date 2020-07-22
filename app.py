23676d1ed269c52aa7e90f266bb24575d958653e
unknown <henrywrandall@gmail.com>
2020-07-22 16:54:05 -0400

    added caching

--- app.py
+++ app.py
@@ -58,25 +58,26 @@ else:
                 'CACHE_MEMCACHED_SERVERS': cache_servers.split(','),
                 'CACHE_MEMCACHED_USERNAME': cache_user,
                 'CACHE_MEMCACHED_PASSWORD': cache_pass,
-                'CACHE_OPTIONS': { 'behaviors': {
-                    # Faster IO
-                    'tcp_nodelay': True,
-                    # Keep connection alive
-                    'tcp_keepalive': True,
-                    # Timeout for set/get requests
-                    'connect_timeout': 2000, # ms
-                    'send_timeout': 750 * 1000, # us
-                    'receive_timeout': 750 * 1000, # us
-                    '_poll_timeout': 2000, # ms
-                    # Better failover
-                    'ketama': True,
-                    'remove_failed': 1,
-                    'retry_timeout': 2,
-                    'dead_timeout': 30}}})
+                # 'CACHE_OPTIONS': { 'behaviors': {
+                #     # Faster IO
+                #     'tcp_nodelay': True,
+                #     # Keep connection alive
+                #     'tcp_keepalive': True,
+                #     # Timeout for set/get requests
+                #     'connect_timeout': 2000, # ms
+                #     'send_timeout': 750 * 1000, # us
+                #     'receive_timeout': 750 * 1000, # us
+                #     '_poll_timeout': 2000, # ms
+                #     # Better failover
+                #     'ketama': True,
+                #     'remove_failed': 1,
+                #     'retry_timeout': 2,
+                #     'dead_timeout': 30}}
+                    })
 
 
 # create route that renders index.html template
-@app.route("/", methods=('POST'))
+@app.route("/", methods=('GET','POST'))
 def home():
     # Pull data from SQL datadase and turn it into JSON
     
