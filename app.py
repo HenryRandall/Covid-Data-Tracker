0acd63ce54f5222313cb428ec1746f334e9a1087
unknown <henrywrandall@gmail.com>
2020-07-22 09:34:45 -0400

    added caching

--- app.py
+++ app.py
@@ -41,29 +41,48 @@ engine1 = create_engine(f'postgresql://{connection1}')
 # connection2=f'{username2}:{password2}@{host2}:{port2}/{database2}'
 # engine2 = create_engine(f'postgresql://{connection2}')
 
-#################################################
-# Caching
-#################################################
-cache=Cache()
-# app.config['CACHE_TYPE']='memcached'
-# cache.init_app(app)
 
-cache_servers = os.environ.get('MEMCACHIER_SERVERS')
-if cache_servers == None:
-    cache.init_app(app, config={'CACHE_TYPE': 'simple'})
-else:
-    cache_user = os.environ.get('MEMCACHIER_USERNAME') or ''
-    cache_pass = os.environ.get('MEMCACHIER_PASSWORD') or ''
-    cache.init_app(app,
-        config={'CACHE_TYPE': 'saslmemcached',
-                'CACHE_MEMCACHED_SERVERS': cache_servers.split(','),
-                'CACHE_MEMCACHED_USERNAME': cache_user,
-                'CACHE_MEMCACHED_PASSWORD': cache_pass})
+
+# #################################################
+# # Caching
+# #################################################
+# cache=Cache()
+# # app.config['CACHE_TYPE']='memcached'
+# # cache.init_app(app)
+
+# cache_servers = os.environ.get('MEMCACHIER_SERVERS')
+# if cache_servers == None:
+#     cache.init_app(app, config={'CACHE_TYPE': 'simple'})
+# else:
+#     cache_user = os.environ.get('MEMCACHIER_USERNAME') or ''
+#     cache_pass = os.environ.get('MEMCACHIER_PASSWORD') or ''
+#     cache.init_app(app,
+#         config={'CACHE_TYPE': 'saslmemcached',
+#                 'CACHE_MEMCACHED_SERVERS': cache_servers.split(','),
+#                 'CACHE_MEMCACHED_USERNAME': cache_user,
+#                 'CACHE_MEMCACHED_PASSWORD': cache_pass,
+#                 'CACHE_OPTIONS': { 'behaviors': {
+#                     # Faster IO
+#                     'tcp_nodelay': True,
+#                     # Keep connection alive
+#                     'tcp_keepalive': True,
+#                     # Timeout for set/get requests
+#                     'connect_timeout': 2000, # ms
+#                     'send_timeout': 750 * 1000, # us
+#                     'receive_timeout': 750 * 1000, # us
+#                     '_poll_timeout': 2000, # ms
+#                     # Better failover
+#                     'ketama': True,
+#                     'remove_failed': 1,
+#                     'retry_timeout': 2,
+#                     'dead_timeout': 30}}})
+
+
 
 
 # create route that renders index.html template
 @app.route("/")
-@cache.cached(timeout=3600)
+# @cache.cached(timeout=3600)
 def home():
     # Pull data from SQL datadase and turn it into JSON
     state_heatmap=pd.read_sql_query('select * from state_heatmap', con=engine1)
@@ -105,21 +124,3 @@ def aboutus():
 
 if __name__ == "__main__":
     app.run(debug=False)
-
-
-# ,
-#                 'CACHE_OPTIONS': { 'behaviors': {
-#                     # Faster IO
-#                     'tcp_nodelay': True,
-#                     # Keep connection alive
-#                     'tcp_keepalive': True,
-#                     # Timeout for set/get requests
-#                     'connect_timeout': 2000, # ms
-#                     'send_timeout': 750 * 1000, # us
-#                     'receive_timeout': 750 * 1000, # us
-#                     '_poll_timeout': 2000, # ms
-#                     # Better failover
-#                     'ketama': True,
-#                     'remove_failed': 1,
-#                     'retry_timeout': 2,
-#                     'dead_timeout': 30}}
