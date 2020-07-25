f13d33183b68c97328ba5acd4a11552094ed9569
unknown <henrywrandall@gmail.com>
2020-07-24 20:43:04 -0400

    added methodology page

--- app.py
+++ app.py
@@ -70,86 +70,86 @@ else:
                     'retry_timeout': 2,
                     'dead_timeout': 30}}
                     })
-##############################################################
-# Pull data from SQL datadase and turn it into JSON and cache
-##############################################################
-state_heatmap=cache.get('state_heatmap')
-if state_heatmap==None:
-    state_heatmap=pd.read_sql_query('select * from state_heatmap', con=engine1)
-    state_heatmap=state_heatmap.to_json(orient='records')
-    cache.set('state_heatmap',state_heatmap)
-    print('state_heatmap cached')
-
-usa_heatmap=cache.get('usa_heatmap')
-if usa_heatmap==None:
-    usa_heatmap=pd.read_sql_query('select * from usa_heatmap', con=engine1)
-    usa_heatmap=usa_heatmap.to_json(orient='records')
-    cache.set('usa_heatmap',usa_heatmap)
-    print('state_heatmap cached')
-
-county_heatmap=cache.get('county_heatmap')
-if county_heatmap==None:
-    county_heatmap=pd.read_sql_query('select * from county_heatmap', con=engine1)
-    county_heatmap=county_heatmap.to_json(orient='records')
-    # Fix Parsing error where python and javascript look at apostrophes in different ways
-    county_heatmap=county_heatmap.replace("'",r"\'")
-    cache.set('county_heatmap',county_heatmap)
-    print('state_heatmap cached')
-
-orders=cache.get('orders')
-if orders==None:
-    orders=pd.read_sql_query('select * from orders', con=engine1)
-    orders=orders.to_json(orient='records')
-    cache.set('orders',orders)
-    print('state_heatmap cached')
-
-state_cases=cache.get('state_cases')
-if state_cases==None:
-    state_cases=pd.read_sql_query('select * from state_cases', con=engine1)
-    state_cases=state_cases.to_json(orient='records')
-    cache.set('state_cases',state_cases)
-    print('state_heatmap cached')
-
-state_deaths=cache.get('state_deaths')
-if state_deaths==None:
-    state_deaths=pd.read_sql_query('select * from state_deaths', con=engine1)
-    state_deaths=state_deaths.to_json(orient='records')
-    cache.set('state_deaths',state_deaths)
-    print('state_heatmap cached')
-
-compressed_county_cases=cache.get('compressed_county_cases')
-if compressed_county_cases==None:
-    county_cases=pd.read_sql_query('select * from county_cases', con=engine1)
-    county_cases=county_cases.to_json(orient='records')
-    # Fix Parsing error where python and javascript look at apostrophes in different ways
-    county_cases=county_cases.replace("'",r"\'")
-    compressed_county_cases=zlib.compress(county_cases.encode('utf8'), level=9)
-    cache.set('compressed_county_cases',compressed_county_cases)
-    print('state_heatmap cached')
-else:
-    county_cases=zlib.decompress(compressed_county_cases).decode('utf8')
-
-compressed_county_deaths=cache.get('compressed_county_deaths')
-if compressed_county_deaths==None:
-    county_deaths=pd.read_sql_query('select * from county_deaths', con=engine1)
-    county_deaths=county_deaths.to_json(orient='records')
-    # Fix Parsing error where python and javascript look at apostrophes in different ways
-    county_deaths=county_deaths.replace("'",r"\'")
-    compressed_county_deaths=zlib.compress(county_deaths.encode('utf8'), level=9)
-    cache.set('compressed_county_deaths',compressed_county_deaths)
-    print('state_heatmap cached')
-else:
-    county_deaths=zlib.decompress(compressed_county_deaths).decode('utf8')
 
 # create route that renders html templates
 @app.route("/")
-@cache.cached()
 def home():
+    ##############################################################
+    # Pull data from SQL datadase and turn it into JSON and cache
+    ##############################################################
+    state_heatmap=cache.get('state_heatmap')
+    if state_heatmap==None:
+        state_heatmap=pd.read_sql_query('select * from state_heatmap', con=engine1)
+        state_heatmap=state_heatmap.to_json(orient='records')
+        cache.set('state_heatmap',state_heatmap)
+        print('state_heatmap cached')
+
+    usa_heatmap=cache.get('usa_heatmap')
+    if usa_heatmap==None:
+        usa_heatmap=pd.read_sql_query('select * from usa_heatmap', con=engine1)
+        usa_heatmap=usa_heatmap.to_json(orient='records')
+        cache.set('usa_heatmap',usa_heatmap)
+        print('state_heatmap cached')
+
+    county_heatmap=cache.get('county_heatmap')
+    if county_heatmap==None:
+        county_heatmap=pd.read_sql_query('select * from county_heatmap', con=engine1)
+        county_heatmap=county_heatmap.to_json(orient='records')
+        # Fix Parsing error where python and javascript look at apostrophes in different ways
+        county_heatmap=county_heatmap.replace("'",r"\'")
+        cache.set('county_heatmap',county_heatmap)
+        print('state_heatmap cached')
     return render_template("index.html", state_heatmap=state_heatmap, usa_heatmap=usa_heatmap, county_heatmap=county_heatmap, API_KEY=API_KEY)
 
 @app.route("/plots")
-@cache.cached()
 def plots():
+    ##############################################################
+    # Pull data from SQL datadase and turn it into JSON and cache
+    ##############################################################
+    orders=cache.get('orders')
+    if orders==None:
+        orders=pd.read_sql_query('select * from orders', con=engine1)
+        orders=orders.to_json(orient='records')
+        cache.set('orders',orders)
+        print('state_heatmap cached')
+
+    state_cases=cache.get('state_cases')
+    if state_cases==None:
+        state_cases=pd.read_sql_query('select * from state_cases', con=engine1)
+        state_cases=state_cases.to_json(orient='records')
+        cache.set('state_cases',state_cases)
+        print('state_heatmap cached')
+
+    state_deaths=cache.get('state_deaths')
+    if state_deaths==None:
+        state_deaths=pd.read_sql_query('select * from state_deaths', con=engine1)
+        state_deaths=state_deaths.to_json(orient='records')
+        cache.set('state_deaths',state_deaths)
+        print('state_heatmap cached')
+
+    compressed_county_cases=cache.get('compressed_county_cases')
+    if compressed_county_cases==None:
+        county_cases=pd.read_sql_query('select * from county_cases', con=engine1)
+        county_cases=county_cases.to_json(orient='records')
+        # Fix Parsing error where python and javascript look at apostrophes in different ways
+        county_cases=county_cases.replace("'",r"\'")
+        compressed_county_cases=zlib.compress(county_cases.encode('utf8'), level=9)
+        cache.set('compressed_county_cases',compressed_county_cases)
+        print('state_heatmap cached')
+    else:
+        county_cases=zlib.decompress(compressed_county_cases).decode('utf8')
+
+    compressed_county_deaths=cache.get('compressed_county_deaths')
+    if compressed_county_deaths==None:
+        county_deaths=pd.read_sql_query('select * from county_deaths', con=engine1)
+        county_deaths=county_deaths.to_json(orient='records')
+        # Fix Parsing error where python and javascript look at apostrophes in different ways
+        county_deaths=county_deaths.replace("'",r"\'")
+        compressed_county_deaths=zlib.compress(county_deaths.encode('utf8'), level=9)
+        cache.set('compressed_county_deaths',compressed_county_deaths)
+        print('state_heatmap cached')
+    else:
+        county_deaths=zlib.decompress(compressed_county_deaths).decode('utf8')
     return render_template("plots.html", orders=orders, state_cases=state_cases, state_deaths=state_deaths, county_cases=county_cases, county_deaths=county_deaths)
 
 @app.route("/methodology")
